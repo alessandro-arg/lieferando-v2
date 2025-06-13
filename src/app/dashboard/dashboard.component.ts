53de93f2018d4ec019c6032b6f27caef561658ac
Alessandro Argenziano <1603alessandro@gmail.com>
2025-06-13 20:00:37 +0200

    worked on the layout of the dashboard content, the cart and the restaurant component are now working correctly

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+} from '@angular/core';
+import { CartComponent } from '../cart/cart.component';
+import { RestaurantComponent } from '../restaurant/restaurant.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RestaurantComponent, CartComponent, CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements AfterViewInit {
+  isFixed = false;
+  isFooterVisible = false;
 
+  @ViewChild('footer', { static: false }) footerRef!: ElementRef;
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    this.isFixed = scrollTop > 72;
+
+    const footer = document.querySelector('app-footer');
+    if (footer) {
+      const footerRect = footer.getBoundingClientRect();
+      this.isFooterVisible = footerRect.top < window.innerHeight;
+    }
+  }
+
+  ngAfterViewInit() {
+    this.onScroll();
+  }
 }
