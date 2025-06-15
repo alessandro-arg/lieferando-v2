fce2945c29e6603727a19e5c3ee76b3d1933fbe3
Alessandro Argenziano <1603alessandro@gmail.com>
2025-06-16 00:18:07 +0200

    making good progress with the cart component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
 })
 export class CartComponent {
+  selected = signal<'delivery' | 'collection'>('delivery');
 
+  setMode(mode: 'delivery' | 'collection') {
+    this.selected.set(mode);
+  }
 }
