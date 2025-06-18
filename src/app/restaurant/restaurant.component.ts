58ac9dae5ac41d80f743cfcd5410fd47dd848e59
Alessandro Argenziano <1603alessandro@gmail.com>
2025-06-18 18:17:26 +0200

    added some documents to the database, created a sticky wrapper to that the search bar follows the user on scroll down

--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -7,12 +7,19 @@ import {
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgFor } from '@angular/common';
+import { HostListener } from '@angular/core';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatTabsModule } from '@angular/material/tabs';
+import {
+  MatTabChangeEvent,
+  MatTabGroup,
+  MatTabsModule,
+} from '@angular/material/tabs';
+
 import {
   Firestore,
   collection,
@@ -38,13 +45,23 @@ import {
   styleUrl: './restaurant.component.scss',
 })
 export class RestaurantComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatTabGroup) tabGroup!: MatTabGroup;
   @ViewChild('scrollContainer', { static: false }) scrollContainer!: ElementRef;
 
   highlights: any[] = [];
   categories: any[] = [];
+  tabLabels: string[] = [
+    'Highlights',
+    'Offers',
+    'Pasta',
+    'Sopus',
+    'Salads',
+    'Pizza',
+  ];
 
   showLeftArrow = false;
   showRightArrow = true;
+  isStuck = false;
 
   constructor(private firestore: Firestore) {}
 
@@ -82,6 +99,39 @@ export class RestaurantComponent implements AfterViewInit, OnInit {
     setTimeout(() => this.checkScroll(), 0);
   }
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    let currentIndex = 0;
+
+    for (let i = 0; i < this.tabLabels.length; i++) {
+      const id = this.sanitizeId(this.tabLabels[i]);
+      const el = document.getElementById(id);
+
+      if (el) {
+        const rect = el.getBoundingClientRect();
+        if (rect.top <= 120 && rect.bottom >= 120) {
+          currentIndex = i;
+          break;
+        }
+      }
+    }
+
+    if (this.tabGroup.selectedIndex !== currentIndex) {
+      this.tabGroup.selectedIndex = currentIndex;
+    }
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const stickyTarget = document.querySelector('.sticky-wrapper');
+    const hero = document.querySelector('.hero');
+
+    if (stickyTarget && hero) {
+      const heroBottom = hero.getBoundingClientRect().bottom;
+      this.isStuck = heroBottom <= 0;
+    }
+  }
+
   scrollLeft() {
     this.scrollContainer.nativeElement.scrollBy({
       left: -200,
@@ -101,5 +151,20 @@ export class RestaurantComponent implements AfterViewInit, OnInit {
     this.showLeftArrow = container.scrollLeft > 0;
     this.showRightArrow =
       container.scrollLeft + container.clientWidth < container.scrollWidth;
+  }
+
+  sanitizeId(name: string): string {
+    return name.toLowerCase().replace(/[^a-z0-9]+/g, '-');
+  }
+
+  onTabChange(event: MatTabChangeEvent): void {
+    const selectedLabel = this.tabLabels[event.index];
+    const id = this.sanitizeId(selectedLabel);
+    const el = document.getElementById(id);
+    if (el) {
+      const yOffset = -180;
+      const y = el.getBoundingClientRect().top + window.scrollY + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
   }
 }
